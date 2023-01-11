6edd44a31581a4f2d8d5495422a048ef8c95f738
Jeonghun Kim <zeonghun413@gmail.com>
2023-01-11 13:52:56 +0900

    calling the weather by current location

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 // 유저는 현재위치의 날씨를 볼 수 있다(지역, 기온, 날씨 상태)
 // 유저는 다른 도시의 버튼들을 볼 수 있다
@@ -6,7 +7,28 @@ import "./App.css";
 // 유저는 데이터가 로딩될 때 로딩 스피너를 볼 수 있다
 
 function App() {
-  return <div></div>;
+  const getCurrentLocation = () => {
+    // 현재 위치 가져오기
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude; // 위도
+      let lon = position.coords.longitude; // 경도
+      getWeatherByCurrentLocation(lat, lon);
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    // 현재 위치 기반 날씨 가져오기
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c1bf7f1a52627b76245947adeac87b7e`;
+    let response = await fetch(url); // API 호출
+    let data = await response.json();
+    console.log("data", data);
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  return <div>weather-app</div>;
 }
 
 export default App;
