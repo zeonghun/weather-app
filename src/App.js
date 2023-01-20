2f593e9acc9c46d7e81649f8ec72a2ae2eb8de58
Jeonghun Kim <zeonghun413@gmail.com>
2023-01-20 16:29:54 +0900

    loading spinner

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 import WeatherBox from "./component/WeatherBox";
 import WeatherButton from "./component/WeatherButton";
+import ClipLoader from "react-spinners/ClipLoader";
 
 // 유저는 현재위치의 날씨를 볼 수 있다(지역, 기온, 날씨 상태)
 // 유저는 다른 도시의 버튼들을 볼 수 있다
@@ -12,6 +13,7 @@ import WeatherButton from "./component/WeatherButton";
 function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
   const cities = ["New York", "London", "Dubai", "Seoul"];
   const getCurrentLocation = () => {
     // 현재 위치 가져오기
@@ -25,17 +27,21 @@ function App() {
   const getWeatherByCurrentLocation = async (lat, lon) => {
     // 현재 위치 기반 날씨 가져오기
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c1bf7f1a52627b76245947adeac87b7e&units=metric`;
+    setLoading(true);
     let response = await fetch(url); // API 호출
     let data = await response.json();
     setWeather(data);
+    setLoading(false);
   };
 
   const getWeatherByCity = async () => {
     // 도시별 날씨 가져오기
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c1bf7f1a52627b76245947adeac87b7e&units=metric`;
+    setLoading(true);
     let response = await fetch(url); // API 호출
     let data = await response.json();
     setWeather(data);
+    setLoading(false);
   };
 
   /*
@@ -60,10 +66,17 @@ function App() {
 
   return (
     <div>
-      <div className="container">
-        <WeatherBox weather={weather} />
-        <WeatherButton cities={cities} setCity={setCity} />
-      </div>
+      {loading ? (
+        <div className="container">
+          <ClipLoader color="#f88c6b" loading={loading} size={150} />
+        </div>
+      ) : (
+        <div className="container">
+          <WeatherBox weather={weather} />
+          <WeatherButton cities={cities} setCity={setCity} />
+        </div>
+      )}
+      {/* loading이 true이면 로딩 스피너를 보여주고, false이면 날씨정보를 보여줌 */}
     </div>
   );
 }
